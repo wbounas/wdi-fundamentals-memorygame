016a69c35559181b6fe4aa7013102eb9b4341b57
Will <wbounas@gmail.com>
2017-11-25 21:49:47 -0500

    finished memory game without reset and score

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,7 +33,9 @@ var checkForMatch = function () {
 	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+
 	console.log('User flipped ' + cards[cardId].rank);
 
 	cardsInPlay.push(cards[cardId].rank);
@@ -41,13 +43,43 @@ var flipCard = function (cardId) {
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+var startOver = document.getElementById('reset');
+startOver.setAttribute('onclick', resetGame);
+startOver.setAttribute('value', 'Reset');
+document.getElementById('game-board').appendChild(startOver);
+
+var resetGame = function () {
+	for (var i = cards.length; i > 0; i--) {
+		cardsInPlay.pop();
+	}
+};
+
+var scoreBoard = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		// Add a point to the player's score
+	} else {
+		// Subtract a point to the player's score
+	}
+}
+
+createBoard();
 
 
 
